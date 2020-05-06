5da464e6017b870ce6bebe108f99e3abbd0a4b95
zhengzeqin <zhengzeqin@addcn.com>
2020-05-06 18:44:21 +0800

    +++ 第六章

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutterapp/ThirdClass/ThirdClassApp.dart';
 import 'FifthClass/FifthClassApp.dart';
 import 'FourthClass/FourthClassApp.dart';
 import 'SevenClass/SevenClassApp.dart';
-void main() => runApp(FifthClassApp());
+import 'SixthClass/SixthClassApp.dart';
+void main() => runApp(SixthClassApp());
 
 
 

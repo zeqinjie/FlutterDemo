a536a18641c84a232832b893a1e16bb8177bfda8
zhengzeqin <1875193628@qq.com>
2020-05-17 18:08:19 +0800

    +++

--- flutter_app/lib/SevenClass/MyProviderWidget.dart
+++ flutter_app/lib/SevenClass/MyProviderWidget.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 // 一个通用的InheritedWidget，保存任需要跨组件共享的状态
 class MyProviderInheritedWidget<T> extends InheritedWidget {
   MyProviderInheritedWidget({@required this.data, Widget child}) : super(child: child);
+
   //共享状态使用泛型
   final T data;
+
   @override
   bool updateShouldNotify(MyProviderInheritedWidget<T> old) {
     //在此简单返回true，则每次更新都会调用依赖其的子孙节点的`didChangeDependencies`。
@@ -14,32 +16,41 @@ class MyProviderInheritedWidget<T> extends InheritedWidget {
 }
 
 class MyChangeNotifierProviderWidget<T extends ChangeNotifier>  extends StatefulWidget {
-  MyChangeNotifierProviderWidget({Key key,this.child,this.data}) : super(key: key);
+  MyChangeNotifierProviderWidget({
+    Key key,
+    this.data,
+    this.child,
+  });
 
   final Widget child;
   final T data;
 
-  @override
-  _MyChangeNotifierProviderWidgetState createState() {
-    return _MyChangeNotifierProviderWidgetState();
-  }
-
-  static T of<T>(BuildContext context){
+  //定义一个便捷方法，方便子树中的widget获取共享数据
+  static T of<T>(BuildContext context) {
+//    final type = _typeOf<InheritedProvider<T>>();
     final provider =  context.dependOnInheritedWidgetOfExactType<MyProviderInheritedWidget<T>>();
     return provider.data;
   }
 
+  @override
+  _MyChangeNotifierProviderWidgetState<T> createState() => _MyChangeNotifierProviderWidgetState<T>();
+
 }
 
 class _MyChangeNotifierProviderWidgetState<T extends ChangeNotifier> extends State<MyChangeNotifierProviderWidget<T>> {
+  void update() {
+    //如果数据发生变化（model类调用了notifyListeners），重新构建InheritedProvider
+    setState(() => {});
+  }
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MyProviderInheritedWidget(
-      data: widget.data,
-      child: widget.child,
-    );
+  void didUpdateWidget(MyChangeNotifierProviderWidget<T> oldWidget) {
+    //当Provider更新时，如果新旧数据不"=="，则解绑旧数据监听，同时添加新数据监听
+    if (widget.data != oldWidget.data) {
+      oldWidget.data.removeListener(update);
+      widget.data.addListener(update);
+    }
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
@@ -56,25 +67,14 @@ class _MyChangeNotifierProviderWidgetState<T extends ChangeNotifier> extends Sta
     super.dispose();
   }
 
-  //只要在父widget中调用setState，子widget的didUpdateWidget就一定会被调用，不管父widget传递给子widget构造方法的参数有没有改变。
-  //initState -> build  , didUpdateWidget -> build
   @override
-  void didUpdateWidget(MyChangeNotifierProviderWidget<T> oldWidget) {
-    // TODO: implement didUpdateWidget
-    //当Provider更新时，如果新旧数据不"=="，则解绑旧数据监听，同时添加新数据监听
-    if (widget.data != oldWidget.data) {
-      oldWidget.data.removeListener(update);
-      widget.data.addListener(update);
-    }
-    super.didUpdateWidget(oldWidget);
-  }
-
-  void update() {
-    //如果数据发生变化（model类调用了notifyListeners），重新构建InheritedProvider
-    setState(() => {});
+  Widget build(BuildContext context) {
+    return MyProviderInheritedWidget<T>(
+      data: widget.data,
+      child: widget.child,
+    );
   }
 }
-
 
 class Item {
   Item(this.price, this.count);
@@ -102,25 +102,11 @@ class CartModel extends ChangeNotifier {
 }
 
 class MyProviderWidget extends StatefulWidget {
-  MyProviderWidget({Key key}) : super(key: key);
-
   @override
-  _MyProviderWidgetState createState() {
-    return _MyProviderWidgetState();
-  }
+  _MyProviderWidgetState createState() => _MyProviderWidgetState();
 }
 
 class _MyProviderWidgetState extends State<MyProviderWidget> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -130,7 +116,7 @@ class _MyProviderWidgetState extends State<MyProviderWidget> {
           return Column(
             children: <Widget>[
               Builder(builder: (context){
-                var cart = MyChangeNotifierProviderWidget.of<CartModel>(context);
+                var cart=MyChangeNotifierProviderWidget.of<CartModel>(context);
                 return Text("总价: ${cart.totalPrice}");
               }),
               Builder(builder: (context){
@@ -150,9 +136,3 @@ class _MyProviderWidgetState extends State<MyProviderWidget> {
     );
   }
 }
-
-
-
-
-
-

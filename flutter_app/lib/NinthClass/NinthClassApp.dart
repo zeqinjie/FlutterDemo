478045cf0f52c79dad3f294b5f520c875cbf011c
zhengzeqin <zhengzeqin@addcn.com>
2020-06-04 15:26:37 +0800

    +++ 第九章

---
+++ flutter_app/lib/NinthClass/NinthClassApp.dart
@@ -0,0 +1,40 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:flutterapp/FourthClass/MyAlignWidget.dart';
+
+//课程文章  https://book.flutterchina.club/chapter9/
+class NinthClassApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NinthClassApp DEMO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '第九章'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child:MyAlignWidget(),
+      ),
+    );
+  }
+}

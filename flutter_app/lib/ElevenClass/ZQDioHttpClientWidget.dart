f67a01831dfe875a1a1a43005089e2e722c344f6
zhengzeqin <zhengzeqin@addcn.com>
2020-06-17 15:52:33 +0800

    +++ Dio 网络库

---
+++ flutter_app/lib/ElevenClass/ZQDioHttpClientWidget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ZQDioHttpClientWidget extends StatefulWidget {
+  ZQDioHttpClientWidget({Key key}) : super(key: key);
+
+  @override
+  _ZQDioHttpClientWidgetState createState() {
+    return _ZQDioHttpClientWidgetState();
+  }
+}
+/*
+* dio是一个强大的Dart Http请求库，支持Restful API、FormData、拦截器、请求取消、Cookie管理、文件上传/下载、超时等。https://github.com/flutterchina/dio
+* dio的使用方式随着其版本升级可能会发生变化，如果本节所述内容和dio官方有差异，请以dio官方文档为准
+* */
+class _ZQDioHttpClientWidgetState extends State<ZQDioHttpClientWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
